e496d12c8fa9149d30d67c5ca5267a8566257f7e
Joshua A. Ardito <jardito@thranduil.rel.ri.cmu.edu>
2017-05-01 16:23:13 -0400

    added circle drawing to drawer

--- Drawer.py
+++ Drawer.py
@@ -2,6 +2,8 @@
 import argparse
 import cv2
 import numpy as np
+import math
+
 from time import *
 IMG_WIN = "drawer"
 
@@ -22,6 +24,10 @@ EPSILON = 20
 # initialize the list of reference points and boolean indicating
 # whether cropping is being performed or not
 
+SHAPE_NONE = 0
+SHAPE_CIRCLE = 1
+SHAPE_LINE = 2
+
 badStart = False
 
 def scaleDownPoint(p):
@@ -33,8 +39,8 @@ def close(x1,x2): return abs(x1-x2) < EPSILON
 
 class Drawer:
     def __init__(self):
-        self.refPt = []
-        self.lines = []
+        self.origin = None
+        self.shapes = []
         self.done = False
 
         cv2.namedWindow(IMG_WIN)
@@ -58,10 +64,11 @@ class Drawer:
         )
 
     def snapToLast(self,x,y):
-        if(len(self.lines)==0):
+        if(len(self.shapes)==0):
             return (x,y)
         else:
-            (prev_x,prev_y) = self.lines[-1][1]
+            (p1,p2, _ ) = self.shapes[-1]
+            (prev_x, prev_y) = p1
             if (close(prev_x,x) and close(prev_y,y)):
                 return (prev_x, prev_y)
         return (x,y)
@@ -75,9 +82,12 @@ class Drawer:
     def parseLines(self):
 
         real_lines = []
-        for line in self.lines:
+        (prev_x,prev_y) = (0,0)
+
+        for (p1,p2, shape) in self.shapes:
+
             ((x1,y1),(x2,y2)) = (
-                scaleDownPoint(line[0]),scaleDownPoint(line[1])
+                scaleDownPoint(p1),scaleDownPoint(p2)
             )
 
             # flip x and y because cozmo has x as straight ahead
@@ -86,8 +96,10 @@ class Drawer:
             # y negated because negative y is to Cozmo's right at start.
             # and we start cozmo to the left of the paper.
 
-            real_lines.append((-y1,x1,False))
-            real_lines.append((-y2,x2, True))
+            if (prev_x != x1 or prev_y != y1):
+                real_lines.append((y1,-x1, SHAPE_NONE))
+            real_lines.append((y2,-x2, shape))
+            (prev_x, prev_y) = (x2, y2)
 
         return real_lines
 
@@ -95,21 +107,30 @@ class Drawer:
 
         global badStart
 
-        if (event == cv2.EVENT_LBUTTONDOWN):
+        if (event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_RBUTTONDOWN):
             if (self.inBounds(x,y)):
                 (x,y) = self.snapToLast(x,y)
-                self.refPt = [(x, y)]
+                self.origin = (x, y)
             else:
                 badStart = True
 
-        elif (event == cv2.EVENT_LBUTTONUP):
+        elif (event == cv2.EVENT_LBUTTONUP or event == cv2.EVENT_RBUTTONUP):
             if (self.inBounds(x,y) and (not badStart)):
-                self.refPt.append((x , y))
-                cv2.line(
-                    self.image, self.refPt[0], self.refPt[1], (0, 255, 0), 2
-                )
-                self.lines.append(self.refPt)
-            self.refPt = []
+
+                if (event == cv2.EVENT_RBUTTONUP):
+                    shape = SHAPE_CIRCLE
+                    (cx,cy) = self.origin
+                    radius = int(math.sqrt((cx - x)**2 + (cy - y)**2))
+                    print("Drawing circle with radius: ", radius)
+                    cv2.circle(self.image, (cx,cy), radius, 2, 2)
+                else:
+                    shape = SHAPE_LINE
+                    cv2.line(
+                        self.image, self.origin, (x,y), 2, 2
+                    )
+
+                self.shapes.append((self.origin, (x, y), shape))
+            self.origin = None
             badStart = False
 
         elif event == cv2.EVENT_MBUTTONDOWN:
@@ -119,7 +140,7 @@ class Drawer:
 
 
     # Use this function in fsm's where the waitKey is called externally
-    def getLines(self, wait=False):
+    def getShapes(self, wait=False):
         while True:
             if(self.done):
                 cv2.destroyWindow(IMG_WIN)
